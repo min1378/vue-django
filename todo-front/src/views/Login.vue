cc46a39e00915f206d294fe663b282ce090265f4
min1378 <qwes123@naver.com>
2019-11-18 16:52:02 +0900

    Implement LoginForm:

--- todo-front/src/views/Login.vue
+++ todo-front/src/views/Login.vue
@@ -1,12 +1,20 @@
 <template>
   <div>
     로그인 페이지입니다.
+    <LoginForm/>
   </div>
 </template>
 
 <script>
-export default {
+// 1.  컴포넌트 호출
+import LoginForm from '@/components/LoginForm'
 
+export default {
+  name:"Login",
+  components:{
+    //2. 컴포넌트 등록
+    LoginForm
+  }
 }
 </script>
 
