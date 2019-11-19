92c68dad94fe41455de90b073e4b20e399c8115d
min1378 <qwes123@naver.com>
2019-11-19 17:58:26 +0900

    fullstack

--- todo-front/src/App.vue
+++ todo-front/src/App.vue
@@ -2,6 +2,7 @@
  <div id="app">
    <div id="nav">
      <!-- 컴포넌트가 화면에 보여주도록 하기 위해 router-link 를 해주었지만 logout은 화면에 보여주는게 아닌 하나의 기능이기 때문에 a-tag로 해도 상관없다. -->
+
      <!-- isLoggedIn 값에 따라서 조건부 렌더링 -->
      <div v-if="isLoggedIn">
        <router-link to="/">Home</router-link> |
@@ -9,6 +10,7 @@
      <!-- prevent 를 사용하는 이유는 href 로 redirect 를 방지하기 위함 -->
      <a @click.prevent="logout" herf="/logout">Logout</a>
      </div>
+
      <div v-else>
        <router-link to="/login">Login</router-link>
      </div>
@@ -19,26 +21,30 @@
  </div>
 </template>
 <script>
-import router from  '@/router'
+import router from '@/router'
 export default {
- name: 'App',
- data() {
-   return {
-     isLoggedIn: this.$session.has('jwt')
-   }
- },
- methods: {
-     logout() {
-         this.$session.destroy()
-         router.push('/login')
-     }
- },
+  name: 'App',
+  data() {
+    return {
+        isLoggedIn: this.$session.has('jwt')
+      }
+  },
+
+  methods: {
+      logout() {
+          this.$session.destroy()
+          router.push('/login')
+      }
+  },
+
+
  // data 에 변화가 일어나는 시점에 실행하는 함수
- updated() {
-   this.isLoggedIn = this.$session.has('jwt')
- }
+  updated() {
+    this.isLoggedIn = this.$session.has('jwt')
+  }
 }
 </script>
+
 <style>
 #app {
  font-family: 'Avenir', Helvetica, Arial, sans-serif;
--- todo-front/src/views/Home.vue
+++ todo-front/src/views/Home.vue
@@ -1,14 +1,31 @@
 <template>
   <div class="home">
-
-
+    <h1>TODO</h1>
+    <TodoInput @createTodo="createTodo"/>
+    <TodoList :todos="todos"/>
+    
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import jwtDecode from 'jwt-decode' // JWT의 payload 값을 해석해서 보여주는 library
+import TodoInput from '@/components/TodoInput'
 import router from '@/router'
+import TodoList from '@/components/TodoList'
+
+
 export default {
   name: 'Home',
+  data() {
+    return {
+      todos: [],
+    }
+  },
+  components:{
+    TodoList,
+    TodoInput,
+  },
   methods: {
     // 사용자 로그인 유무를 확인하여 로그인 되어있지 않을 시 로그인 페이지로 보내겠다.
     checkLoggedIn() {
@@ -16,16 +33,72 @@ export default {
       this.$session.start()
 
       //2. 'jwt'가 있는지 확인하겠다.
-      if(this.$session.has('jwt')) {
+      if(!this.$session.has('jwt')) {
         // 로그인 페이지로 보내겠다.
         router.push('/login')
       }
+    },
+
+    // 우리가 만든 django API 서버로 todos를 달라는 요청을 보낸 뒤
+    // todos data에 할당하는 함수
+
+    getTodo() {
+
+      this.$session.start()
+      const token = this.$session.get('jwt')
+
+      const userId = jwtDecode(token).user_id
+
+      const SERVER_IP = process.env.VUE_APP_SERVER_IP
+
+      const options = {
+        headers: {
+          Authorization: 'JWT ' + token
+        }
+      }
+    
+
+
+
+      axios.get(`${SERVER_IP}/api/v1/users/${userId}/`, options)
+        .then(response => {
+          this.todos = response.data.todo_set
+        })
+        .catch(error => {
+          console.log(error)
+        })
+      },
+      createTodo(title) {
+        this.$session.start()
+        const token = this.$session.get('jwt')
+        const SERVER_IP = process.env.VUE_APP_SERVER_IP
+        const userId = jwtDecode(token).user_id
+        const options = {
+          headers: {
+            Authorization: 'JWT ' + token
+          }
+        }
+        // 요청을 보낼 data를 작성
+        const data = {
+          title, 
+          user:userId
+        }
+
+        axios.post(`${SERVER_IP}/api/v1/todos/`,data, options)
+          .then(response =>{
+            this.todos.push(response.data)
+          })
+          .catch(error => {
+            console.log(error)
+          })
     }
   },
+  
   // Vue가 화면에 그려지면 실행하는 함수.
 
   mounted() {
     this.checkLoggedIn()
+    this.getTodo()
   }
 
 
